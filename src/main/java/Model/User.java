aee933abbbdd8576d1aea219cec854b6c9de5aad
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-03-28 15:59:32 +0200

    Lite klasser

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class User {
+    private String userName, passWord;
+
+    public User(String userName, String passWord){
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+
+}
