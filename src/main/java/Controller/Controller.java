2e512ec1b3e4231133220cb9c2511adb4bc67ce4
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-03-28 16:07:39 +0200

    Lite klasser

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,4 +1,7 @@
 package Controller;
 
 public class Controller {
+    public Controller(){
+
+    }
 }
---
+++ src/main/java/Controller/Main.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        new Controller();
+    }
+}
