4731b682dd4f367c674e2cdd37e67336fdefa4a6
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-03-29 10:13:07 +0200

    User

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -4,9 +4,23 @@ public class User {
     private String userName, passWord;
 
     public User(String userName, String passWord){
-        this.userName = userName;
-        this.passWord = passWord;
+        this.setUserName(userName);
+        this.setPassWord(passWord);
     }
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
