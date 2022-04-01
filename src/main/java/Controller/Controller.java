67bc4f4d9ab6a9eee750ce1d27ceca10d7cc4ca7
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-04-01 07:08:37 +0200

    RS1

--- src/main/java/Controller/Controller.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public class Controller {
-    public Controller(){
-
-    }
-}
--- src/main/java/Controller/Main.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public class Main {
-    public static void main(String[] args) {
-        new Controller();
-    }
-}
