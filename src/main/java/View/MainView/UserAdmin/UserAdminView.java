123f01270464a8b3c589ed422842b9b97e7cb7d1
Mallevs1 <Patrik.brandell@hotmail.com>
2022-04-29 16:07:22 +0200

    1

---
+++ src/main/java/View/MainView/UserAdmin/UserAdminView.java
@@ -0,0 +1,14 @@
+package View.MainView.UserAdmin;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserAdminView extends JComponent implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
