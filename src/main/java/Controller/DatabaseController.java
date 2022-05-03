758e0efb8050cec4d4ec389f6bb906eb421c6a4c
Mallevs1 <Patrik.brandell@hotmail.com>
2022-05-03 10:42:45 +0200

    DBControllerupdate

--- src/main/java/Controller/DatabaseController.java
+++ src/main/java/Controller/DatabaseController.java
@@ -46,6 +46,30 @@ public class DatabaseController {
         con.close();
     }
 
+    public void updateUser(User user) throws SQLException {
+        Connection con = getDBConnection();
+        String QUERY = "UPDATE userid " +
+                "SET firstname = " + fixSQLString(user.getFirstName()) +
+                ", lastname = " + fixSQLString(user.getLastName()) +
+                ", password = " + fixSQLString(user.getPassword()) +
+                ", role = " + fixSQLString(user.getRole()) +
+                "WHERE email = " + fixSQLString(user.getEmail());
+
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(QUERY);
+        con.close();
+        controller.updateUserManager(user);
+    }
+
+    public void deleteUser(User user) throws SQLException {
+        Connection con = getDBConnection();
+        String QUERY = "DELETE FROM userid WHERE email = " + fixSQLString(user.getEmail());
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(QUERY);
+        con.close();
+        controller.updateUserManager(user);
+    }
+
     /**
      * @return ArrayList of userobjects
      * @throws Exception
